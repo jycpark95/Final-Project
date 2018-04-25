69a8575e816a3c333ea6f939102a863e844581ad
Haile Shavers <hshavers@gmail.com>
2018-04-25 15:23:54 -0700

    added title to food

--- app/static/js/recipes.js
+++ app/static/js/recipes.js
@@ -92,7 +92,7 @@ function processData(data){
 		var recipe_link = data.recipes[i]['source_url'];
 		var recipe_id = data.recipes[i]['recipe_id'];
 
-   		htmlElements += '<div class="ind_recipe"> <tr> <td><img id="food_image" id="' + recipe_id + '" src="' + food_img + '"></td><td><a href="'+ recipe_link + '"target="_blank"></td> <td><img class="link" src="./static/img/link2.png" title="See recipe" style="width:20px !important;height:20px !important; border:none"></a></td> <td><button class="thumbtack"><img src="./static/img/tack1.png" style="width:20px !important;height:20px !important; border:none; !important;justify-content:right;"></button></td></tr></div>';
+   		htmlElements += '<div class="ind_recipe"> <tr> <td><img id="food_image" id="' + recipe_id + '" src="' + food_img + '"></td><td><a href="'+ recipe_link + '"target="_blank"></td> <td><img class="link" src="./static/img/link2.png" title="See recipe" style="width:20px !important;height:20px !important; border:none"></a></td> <td><button class="thumbtack"><img src="./static/img/tack1.png" style="width:20px !important;height:20px !important; border:none; !important;justify-content:right;"></button>' + title + '</td></tr></div>';
 
    		// htmlElements += '<div class="food-item"><img id="' + recipe_id + '" src="' + food_img + '"><br><a href="'+ recipe_link +'"target="_blank"><b>Recipe Link</b></a><br><br><a href=./templates/base.html>Save to profile.</a></div>';
 
